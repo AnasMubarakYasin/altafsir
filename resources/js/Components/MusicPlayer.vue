c39718422821e13829f2b1737e0080894fbfe487
WahyuSapurata <wm337708@gmail.com>
2023-03-11 11:35:35 +0800

    refactor MusicPlayer

--- resources/js/Components/MusicPlayer.vue
+++ resources/js/Components/MusicPlayer.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
-        <audio ref="audio" :src="src"></audio>
-        <button
-            class="px-3 py-2 bg-[#466970] text-[15px] flex items-center text-white w-max rounded-lg hover:bg-slate-400 hover:shadow-md hover:scale-[1.02]"
-            @click="togglePlay"
-        >
-            {{ playing ? "Pause" : "Play" }}
-        </button>
-    </div>
+    <audio
+        ref="audio"
+        :src="src"
+        @ended="onAudioEnded"
+        @playing="stopOtherAudio"
+    ></audio>
+    <button
+        class="px-3 py-2 bg-[#466970] text-[15px] flex items-center text-white w-max rounded-lg hover:bg-slate-400 hover:shadow-md hover:scale-[1.02]"
+        @click="togglePlay"
+    >
+        <!-- {{ isPlaying  ? return(<i class="pi pi-file mr-1"></i>) : "Play" }} -->
+        <i v-if="isPlaying" class="pi pi-pause"></i>
+        <i v-else class="pi pi-play"></i>
+    </button>
 </template>
 
 <script>
@@ -18,17 +23,29 @@ export default {
     },
     data() {
         return {
-            playing: false,
+            isPlaying: false,
         };
     },
     methods: {
         togglePlay() {
-            if (this.playing) {
+            if (this.isPlaying) {
                 this.$refs.audio.pause();
             } else {
                 this.$refs.audio.play();
             }
-            this.playing = !this.playing;
+            this.isPlaying = !this.isPlaying;
+        },
+        onAudioEnded() {
+            this.isPlaying = false;
+        },
+        stopOtherAudio() {
+            const audios = document.getElementsByTagName("audio");
+            for (let i = 0; i < audios.length; i++) {
+                if (audios[i] !== this.$refs.audio) {
+                    audios[i].pause();
+                    audios[i].currentTime = 0;
+                }
+            }
         },
     },
 };
